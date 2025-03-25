b3bb9b45bc3e91f94ebb7fa1f99b202f73019526
aboo-anas <muhamumdam@gmail.com>
2025-03-25 14:49:16 +0100

    feat: add go back home btn to error-response page

--- client/components/error-response.tsx
+++ client/components/error-response.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useRoute } from "@/client/contexts";
+import Link from "next/link";
 export default function ErrorResponse() {
   const { errorDataRef } = useRoute();
   const errorData = errorDataRef.current;
@@ -9,6 +10,7 @@ export default function ErrorResponse() {
       <p className="mt-20 text-4xl md:text-8xl">Oops!.... </p>
       <p className="text-xl m-auto p-4 text-gray-600 text-center mt-10">You might not have enough cycle hours to complete this trip or your inputs are incorrect.</p>
       <p className="text-xl m-auto p-4 text-gray-600 text-center mt-10">{errorData}</p>
+      <Link  className="py-[12px] px-[20px] inline-block items-center justify-center bg-[#9E77ED] mt-[10px] text-white disabled:bg-gray-600" href={'/'} >Go back home</Link>
     </div>
   );
 }
